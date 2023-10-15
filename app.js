a799e3113eb4ae0b63de47b68950450ae6aaa813
Ubuntu <ubuntu@ip-172-31-37-66.ap-south-1.compute.internal>
2023-10-15 19:55:23 +0000

    this is demo

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get('/items', (req, res) => {
     { id: 1, name: 'Item 1' },
     { id: 2, name: 'Item 2' },
     { id: 3, name: 'Item 3' },
+    { id: 4, name: 'Item 4' },
   ];
 
   res.json({ message: 'This is the protected resource.', data: data });
